13dcc314b07af73ca82fb81fc29a543e64dd129f
blood8879 <blood8879@gmail.com>
2021-09-11 21:53:37 +0900

    20210911

---
+++ src/controllers/videoController.js
@@ -0,0 +1,46 @@
+const videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 12,
+        id: 1
+    },
+    {
+        title: "2nd Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 2
+    },
+    {
+        title: "3rd Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 3
+    },
+];
+
+export const trending = (req, res) =>  {     
+    res.render("home", { pageTitle: "Home", videos });
+}
+export const watch = (req, res) => { 
+    const { id } = req.params;
+    const video = videos[id-1]
+    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
+}
+export const getEdit = (req, res) => { 
+    const { id } = req.params;
+    const video = videos[id-1]
+    return res.render("edit", { pageTitle: `Editing:  ${video.title}`, video}); 
+}
+export const postEdit = (req, res) => {
+
+}
+// export const search = (req, res) => res.send("search");
+// export const upload = (req, res) => res.send("upload");
+// export const deleteVideo = (req, res) => { res.send("delete"); }
--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
-import express from "express";
+import express, { response } from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-app.get("/", () => console.log("Somebody is trying to go home."));
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening =  () => console.log(`Server listening on port http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening)
-
+app.listen(PORT, handleListening);
