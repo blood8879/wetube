8893b86da7d6dc784b0f278d34f08a69fa78da4e
blood8879 <blood8879@gmail.com>
2021-09-22 17:13:33 +0900

    20210922

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { getEdit, postEdit, upload, deleteVideo, watch } from "../controllers/videoController";
+import { getEdit, postEdit, upload, deleteVideo, watch, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-// videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-// videoRouter.get("/upload", upload);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
