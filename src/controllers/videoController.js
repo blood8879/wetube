a926523b70b3a4f6761a1d98d9564dcf565fb136
blood8879 <blood8879@gmail.com>
2021-09-25 21:40:41 +0900

    20210925

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -7,7 +7,7 @@ const handleSearch = (error, videos) => {
 
 export const home = async(req, res) =>  {
     try {
-        const videos = await Video.find({});
+        const videos = await Video.find({}).sort({ createdAt:"desc" });
         return res.render("home", { pageTitle: "Home", videos });    
     } catch {
         return res.render("server-error");
@@ -69,4 +69,19 @@ export const deleteVideo = async(req, res) => {
     const { id } = req.params;
     await Video.findByIdAndDelete(id);
     return res.redirect("/");
+}
+
+export const search = async(req, res) => {
+    const { keyword } = req.query;
+    let videos = [];
+    if (keyword) {
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(keyword, "i"),
+                
+            },
+        });
+        console.log("videos??", videos)     
+    }
+    return res.render("search", { pageTitle: "Search", videos });
 }
